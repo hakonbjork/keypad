716373aedae091e252e903e6bb43c462287acd8a
Mithunan Sivakumar <mithunan2522@gmail.com>
2021-03-07 10:03:39 +0100

    Started working on Keypad

--- Keypad.py
+++ Keypad.py
@@ -1,13 +1,25 @@
-import GPIOSimulator_v5 as GPIO
+from GPIOSimulator_v5 import GPIOSimulator
+import time
+
+GPIO = GPIOSimulator()
+
 
 class Keypad:
     """  Keypad """
+
+    def __init__(self):
+        """
+        Init method
+        """
+        self.rowpins = []
+        self.columnpins = []
 
     def setup(self):
         """
         initialize the row pins as outputs and the column pins as inputs.
         :return: None
         """
+
         pass
 
     def do_polling(self):
@@ -16,6 +28,13 @@ class Keypad:
         pressed on the keypad.
         :return: None
         """
+        for rp in enumerate(self.rowpins):
+            # setting the rowpin HIGH one at a time.
+            GPIO.output(rp, GPIO.HIGH)
+
+            for cp in enumerate(self.columnpins):
+                # If column C has a HIGH reading, then the key at location (R,C) is revealed as the one being pressed.
+                pass
 
     def get_next_signal(self):
         """
