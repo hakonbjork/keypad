345e34bb13fe91342f842bb7ae63994277654996
Mithunan Sivakumar <mithunan2522@gmail.com>
2021-03-08 14:24:21 +0100

    kpc changes

--- KPC.py
+++ KPC.py
@@ -2,6 +2,8 @@
 
 from Keypad import Keypad
 from LED_Board import LED_Board
+from FSM import FSM
+from FSMRule import FSMRule
 
 
 class KPC:
@@ -10,10 +12,13 @@ class KPC:
     def __init__(self):
         self.keypad = Keypad()
         self.led_board = LED_Board()
+        self.FSM = FSM()
         self.path_name = "/password.txt"
         self.override_signal = None
         # cumulative password
-        self.CUMP = 1234
+        self.CUMP = ""
+        self.Lid = 0
+        self.Ldur = 0
 
     def reset_passcode_entry(self):
         """ Clear passcode buffer, initiate power_up lightning sequence
@@ -49,5 +54,25 @@ class KPC:
 
         # TODO: Find out whether we should clear override_signal sometime
 
-    def validate_password_change(self):
+    def validate_password_change(self, password):
         """ Check that new password is legal and write it to password file """
+        if len(password) > 4:
+            for i in password:
+                if 0 <= int(i) <= 9:
+                    f = open(self.path_name, "w")
+                    f.write(password)
+                    f.close()
+                    # led lights
+        else:
+            # led lights
+            pass
+
+    def light_one_led(self):
+        self.led_board.user_choose_led(self.Lid, self.Ldur)
+
+    def flash_leds(self):
+        self.led_board.flash_all_leds()
+
+    def exit_action(self):
+        self.led_board.power_down_the_system()
+        self.__init__()
